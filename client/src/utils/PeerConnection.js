1af1da83d1de4b9f4d994a436eb826f6c1a57f8e
Sergey <50526865+Kosrotoff@users.noreply.github.com>
2022-07-27 17:56:02 +0700

    Bugfix

--- client/src/utils/PeerConnection.js
+++ client/src/utils/PeerConnection.js
@@ -2,27 +2,29 @@ import Emitter from './Emitter'
 import MediaDevice from './MediaDevice'
 import socket from './socket'
 
-const CONFIG = { iceServers: [{ urls: ['stun:stun.l.google.com:19302'] }] }
 
-class PeerConnection extends Emitter {
+const CONFIG = {
+    iceServers: [
+        {
+            urls: ['stun:stun.l.google.com:19302']
+        }
+    ]
+};
+
+export default class PeerConnection extends Emitter {
     constructor(remoteId) {
         super()
-        this.remoteId = remoteId
 
-        this.rtcPeerConnection = new RTCPeerConnection(CONFIG)
-        this.rtcPeerConnection.onicecandidate = ({ candidate }) => {
-            socket.emit('call', {
+        this.remoteId = remoteId;
+        this.rtcPeerConnection = new RTCPeerConnection(CONFIG);
+        this.rtcPeerConnection.onicecandidate = ({ candidate }) => socket.emit('call', {
                 to: this.remoteId,
                 candidate
-            })
-        }
-        this.rtcPeerConnection.ontrack = ({ streams }) => {
-            this.emit('remoteStream', streams[0])
-        }
+            });
+        this.rtcPeerConnection.ontrack = ({ streams }) => this.emit('remoteStream', streams[0]);
 
-        this.mediaDevice = new MediaDevice()
-
-        this.getDescription = this.getDescription.bind(this)
+        this.mediaDevice = new MediaDevice();
+        this.getDescription = this.getDescription.bind(this);
     }
 
     start(isCaller, config) {
@@ -30,63 +32,68 @@ class PeerConnection extends Emitter {
             .on('stream', (stream) => {
                 stream.getTracks().forEach((t) => {
                     this.rtcPeerConnection.addTrack(t, stream)
-                })
+                });
 
-                this.emit('localStream', stream)
+                this.emit('localStream', stream);
 
-                isCaller
-                    ? socket.emit('request', { to: this.remoteId })
-                    : this.createOffer()
+                if (isCaller) {
+                    socket.emit('request', { to: this.remoteId });
+                } else {
+                    this.createOffer();
+                }
             })
-            .start(config)
+            .start(config);
 
         return this
     }
 
     stop(isCaller) {
         if (isCaller) {
-            socket.emit('end', { to: this.remoteId })
+            socket.emit('end', { to: this.remoteId });
         }
-        this.mediaDevice.stop()
-        this.rtcPeerConnection.restartIce()
-        this.off()
 
-        return this
+        this.mediaDevice.stop();
+        this.rtcPeerConnection.restartIce();
+        this.off();
+
+        return this;
     }
 
     createOffer() {
-        this.rtcPeerConnection.createOffer().then(this.getDescription).catch(console.error)
+        this.rtcPeerConnection.createOffer()
+            .then(this.getDescription)
+            .catch(console.error);
 
-        return this
+        return this;
     }
 
     createAnswer() {
-        this.rtcPeerConnection.createAnswer().then(this.getDescription).catch(console.error)
+        this.rtcPeerConnection.createAnswer()
+            .then(this.getDescription)
+            .catch(console.error);
 
-        return this
+        return this;
     }
 
     getDescription(desc) {
-        this.rtcPeerConnection.setLocalDescription(desc)
+        this.rtcPeerConnection.setLocalDescription(desc);
 
-        socket.emit('call', { to: this.remoteId, sdp: desc })
+        socket.emit('call', { to: this.remoteId, sdp: desc });
 
-        return this
+        return this;
     }
 
     setRemoteDescription(desc) {
-        this.rtcPeerConnection.setRemoteDescription(new RTCSessionDescription(desc))
+        this.rtcPeerConnection.setRemoteDescription(new RTCSessionDescription(desc));
 
-        return this
+        return this;
     }
 
     addIceCandidate(candidate) {
         if (candidate) {
-            this.rtcPeerConnection.addIceCandidate(new RTCIceCandidate(candidate))
+            this.rtcPeerConnection.addIceCandidate(new RTCIceCandidate(candidate));
         }
 
-        return this
+        return this;
     }
 }
-
-export default PeerConnection
--- client/src/utils/socket.js
+++ client/src/utils/socket.js
@@ -1,9 +1,10 @@
-import { io } from 'socket.io-client'
+import { io } from 'socket.io-client';
 
-// в производственном режиме сервер и клиент будут находиться в одном источнике (origin),
-// а в режиме для разработки - в разных
+
 const SERVER_URI = 'http://localhost:4000';
 
+
 const socket = io(SERVER_URI);
+
 
 export default socket;
